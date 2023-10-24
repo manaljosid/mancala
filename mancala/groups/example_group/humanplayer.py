a659275943d14c8d056686889e8b9a9f74702ff0
Stefanía Bergljót <stefaniabergljot@gmail.com>
2023-10-24 08:58:02 +0000

    Human input playe

--- mancala/groups/example_group/humanplayer.py
+++ mancala/groups/example_group/humanplayer.py
@@ -7,7 +7,6 @@ from typing import Tuple
 
 NAME = "human"
 
-
 def action(board: array.array, legal_actions: Tuple[int, ...], player: int) -> int:
     print("Player " + str(player))
     return int(input("Select move from " + str(legal_actions) + ":"))
